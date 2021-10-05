f3d9482226c84fbe6c4b5f8d4d1f98d24fe67391
Elsueno95 <38080435+Elsueno95@users.noreply.github.com>
2021-10-06 01:01:17 +0300

    Add files via upload

---
+++ Test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Эли Албертов on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let x = 3
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
