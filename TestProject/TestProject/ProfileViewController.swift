0f616be3db2dfc748c23845de39c488ee1fbb306
Your Name <you@example.com>
2021-10-06 23:55:29 +0300

    Добавил новый файл

---
+++ TestProject/TestProject/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  TestProject
+//
+//  Created by Эли Албертов on 06.10.2021.
+//  Copyright © 2021 Эли Албертов. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    // Это мой файл
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
