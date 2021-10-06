c2bbfe30a84a96197c724d0881d82f7fbda0cb92
Your Name <you@example.com>
2021-10-06 23:48:56 +0300

    New Project

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Эли Албертов on 06.10.2021.
+//  Copyright © 2021 Эли Албертов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
