5a4777357e35d7e205a4c2fe747079e267cf3d62
Els <ibadallah95@mail.ru>
2021-10-08 00:43:30 +0300

    Добавил label

--- TestProject/TestProject/ViewController.swift
+++ TestProject/TestProject/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(label)
     }
 
 
